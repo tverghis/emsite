10ddefd44bc05e5d21c1a226a2de21c7f34b4c62
Tarun Verghis <tarun.verghis@gmail.com>
2024-02-16 23:30:45 -0800

    Add upload form

--- handlers/upload.go
+++ handlers/upload.go
@@ -6,19 +6,25 @@ import (
 	"path"
 )
 
-type UploadHandler struct {
+type Upload struct {
 	template template.Template
 }
 
-func NewUploadHandler() *UploadHandler {
+func NewUpload() *Upload {
+	basePath := path.Join("templates", "_base.html")
 	templatePath := path.Join("templates", "upload.html")
-	t := *template.Must(template.ParseFiles(templatePath))
+	t := *template.Must(template.ParseFiles(templatePath, basePath))
 
-	return &UploadHandler{t}
+	return &Upload{t}
 }
 
-func (h *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := h.template.Execute(w, nil); err != nil {
+func (h *Upload) GetUpload(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := h.template.ExecuteTemplate(w, "base", nil); err != nil {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 	}
+}
+
+func (h *Upload) PostUpload(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/upload", http.StatusSeeOther)
 }
--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 const Port = 8080
 
 func main() {
-	uploadHandler := handlers.NewUploadHandler()
-	http.Handle("GET /upload", uploadHandler)
+	uploadHandler := handlers.NewUpload()
+	http.HandleFunc("GET /upload", uploadHandler.GetUpload)
+	http.HandleFunc("POST /upload", uploadHandler.PostUpload)
 
 	fmt.Println("Server listening on port", Port)
 
