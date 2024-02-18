b5344cd84630338d8747a9d38182f88159ba3dc8
Tarun Verghis <tarun.verghis@gmail.com>
2024-02-17 20:11:06 -0800

    Ensure uploads dir exists on startup

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	handlers "github.com/tverghis/emsite/handlers"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
+
+	handlers "github.com/tverghis/emsite/handlers"
 )
 
 const Port = 8080
 
 func main() {
+	if err := ensureUploadsDir(); err != nil && !errors.Is(err, fs.ErrExist) {
+		panic(err)
+	}
+
 	uploadHandler := handlers.NewUpload()
 	http.HandleFunc("GET /upload", uploadHandler.GetUpload)
 	http.HandleFunc("POST /upload", uploadHandler.PostUpload)
@@ -17,4 +25,8 @@ func main() {
 	fmt.Println("Server listening on port", Port)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Port), nil))
+}
+
+func ensureUploadsDir() error {
+	return os.Mkdir("uploads", 0755)
 }
