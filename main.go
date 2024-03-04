7fac4395713f4d84256606d277033e9e708d691b
Tarun Verghis <tarun.verghis@gmail.com>
2024-03-03 22:56:44 -0800

    Run DB migration on startup

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/tverghis/emsite/handlers"
 	"github.com/tverghis/emsite/util/files"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 const Port = 8080
 
 func main() {
+	if err := runDBMigrations(); err != nil {
+		panic(err)
+	}
+
 	if err := files.EnsureUploadsDir(); err != nil {
 		panic(err)
 	}
@@ -33,4 +42,20 @@ func main() {
 	fmt.Println("Server listening on port", Port)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Port), nil))
+}
+
+func runDBMigrations() error {
+	m, err := migrate.New("file://db/migrations", "sqlite3://db/database.db")
+
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	fmt.Println("Successfully ran DB migrations")
+
+	return nil
 }
